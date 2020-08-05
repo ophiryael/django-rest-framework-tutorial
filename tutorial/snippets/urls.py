16691d75fc2df496e606603def4122868b715b60
Ophir Yael <ofir.yael@gmail.com>
2020-08-06 01:32:10 +0300

    Replace explicit url config with implicit router created routes

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,28 +1,11 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from snippets import views
-from snippets.views import SnippetViewSet, UserViewSet, api_root
-from rest_framework import renderers
 
-user_list = UserViewSet.as_view({"get": "list"})
-user_detail = UserViewSet.as_view({"get": "retrieve"})
-snippet_list = SnippetViewSet.as_view({"get": "list", "post": "create"})
-snippet_detail = SnippetViewSet.as_view(
-    {"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"}
-)
-snippet_highlight = SnippetViewSet.as_view(
-    {"get": "highlight"}, renderer_classes=[renderers.StaticHTMLRenderer]
-)
+router = DefaultRouter()
+router.register(r"snippets", views.SnippetViewSet)
+router.register(r"users", views.UserViewSet)
 
-urlpatterns = format_suffix_patterns(
-    [
-        path("", views.api_root),
-        path("users/", user_list, name="user-list"),
-        path("users/<int:pk>", user_detail, name="user-detail"),
-        path("snippets/", snippet_list, name="snippet-list"),
-        path("snippets/<int:pk>/", snippet_detail, name="snippet-detail"),
-        path(
-            "snippets/<int:pk>/highlight/", snippet_highlight, name="snippet-highlight",
-        ),
-    ]
-)
+urlpatterns = [
+    path("", include(router.urls)),
+]
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -3,19 +3,8 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics, permissions, renderers, viewsets
 from snippets.permissions import IsOwnerOrReadOnly
-from rest_framework.decorators import api_view, action
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
-
-
-@api_view(["GET"])
-def api_root(request, format=None):
-    return Response(
-        {
-            "users": reverse("user-list", request=request, format=format),
-            "snippets": reverse("snippet-list", request=request, format=format),
-        }
-    )
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
